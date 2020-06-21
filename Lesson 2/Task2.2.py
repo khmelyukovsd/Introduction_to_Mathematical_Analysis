2696705fbfd4c8999a4813af63cdb01fda979118
khmelyukovsd <khmelyukovsd@yandex.ru>
2020-06-21 21:20:13 +0300

    Homework Lesson 2 task 2.2

--- Lesson 2/Task2.2.py
+++ Lesson 2/Task2.2.py
@@ -1,3 +1,21 @@
 # Даны три множества a,b и с. Необходимо выполнить все изученные виды бинарных операций над всеми комбинациями множеств.
-# *Выполнить задание 1 на языке Python
+# *Выполнить задание на языке Python
 
+a = set('12345')
+b = set('651861521')
+c = set('23')
+
+print('Объединение множеств А и В:', a.union(b))
+print('Пересечение множеств А и В:', a.intersection(b))
+print('Разность множеств А и В:', a.difference(b))
+print('Симметрическая разность множеств А и В:', a.symmetric_difference(b))
+print()
+print('Объединение множеств А и C:', a.union(c))
+print('Пересечение множеств А и C:', a.intersection(c))
+print('Разность множеств А и C:', a.difference(c))
+print('Симметрическая разность множеств А и C:', a.symmetric_difference(c))
+print()
+print('Объединение множеств B и C:', b.union(c))
+print('Пересечение множеств B и C:', b.intersection(c))
+print('Разность множеств B и C:', b.difference(c))
+print('Симметрическая разность множеств B и C:', b.symmetric_difference(c))
